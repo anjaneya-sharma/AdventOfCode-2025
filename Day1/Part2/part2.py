f2f5fccb7e407db3b549e924b8d76f26f0a1b7df
anjaneya_sharma <anjaneya21449@iiitd.ac.in>
2025-12-02 03:25:18 +0530

    Added Day 1 - Part 2 : Correct code

--- Day1/Part2/part2.py
+++ Day1/Part2/part2.py
@@ -8,17 +8,14 @@ with open('../Day1Input.txt', 'r') as f:
         Number = int(line[1:])
         print(f"direction : {Direction}, Number : {Number}")
         position = (position - Number)%100 if Direction == 'L' else (position + Number)%100
-        if position == 0:
-            ans_count+=((Number)//100 + 1) if earlier_position != 0 else (Number//100)
-        elif Direction == 'L':
-            ans_count += ((Number)//100 + int((earlier_position - (Number%100)) < 0))
-            if earlier_position == 0:
-                ans_count -= 1
-        elif Direction == 'R':
-            ans_count += ((Number)//100 + int((earlier_position + (Number%100)) > 100))
-        
-        earlier_poisition = position
+        ans_count += Number//100 
+        if earlier_position != 0:
+            if Direction == 'L':
+                ans_count += 1 if (earlier_position - (Number%100) <= 0) else 0
+            elif Direction == 'R':
+                ans_count += 1 if (earlier_position + (Number%100) >= 100) else 0
 
+        earlier_position = position
 
 
 print(ans_count)
